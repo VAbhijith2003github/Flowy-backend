89035845c1d398e815dac2e53cc826ddcd25f47c
V Abhijith <119759593+VAbhijith2003github@users.noreply.github.com>
2024-03-31 10:29:51 +0530

    adds delete and, put req endpoints for markascomplete and edit

--- routes/task.js
+++ routes/task.js
@@ -62,6 +62,73 @@ router.post("/", (req, res, next) => {
     .catch((err) => next(err));
 });
 
+router.delete("/:taskId", (req, res, next) => {
+  const taskId = req.params.taskId;
+  const DeleteTask = async () => {
+    try {
+      const result = await client.query(
+        "DELETE FROM tasks WHERE string_id = $1",
+        [taskId]
+      );
+      res.status(200).json({ message: "Task deleted successfully" });
+      return result;
+    } catch (err) {
+      console.log(err);
+      throw new Error(err.message);
+    }
+  };
+
+  DeleteTask()
+    .then(() => next())
+    .catch((err) => next(err));
+});
+
+router.put("/updatecompletestatdb/:taskId", (req, res, next) => {
+  const taskId = req.params.taskId;
+  const updatecompletionstatus = async () => {
+    const { completed, dateCompleted } = req.body;
+
+    try {
+      const updatedTask = await client.query(
+        "UPDATE tasks SET completed = $1, dateCompleted = $2 WHERE string_id = $3 RETURNING *",
+        [completed, dateCompleted, taskId]
+      );
+
+      res.json(updatedTask.rows[0]);
+    } catch (err) {
+      console.log(err);
+      throw new Error(err.message);
+    }
+  };
+
+  updatecompletionstatus()
+    .then(() => next())
+    .catch((err) => next(err));
+});
+
+router.put("/edit/:taskId", (req, res, next) => {
+  const taskId = req.params.taskId;
+  const EditTask = async () => {
+    const { title, description, dueDate } = req.body;
+
+    try {
+      const updatedTask = await client.query(
+        "UPDATE tasks SET title = $1, description = $2, dueDate = $3 WHERE string_id = $4 RETURNING *",
+        [title, description, dueDate, taskId]
+      );
+
+      res.json(updatedTask.rows[0]);
+    } catch (err) {
+      console.log(err);
+      throw new Error(err.message);
+    }
+  };
+
+  EditTask()
+    .then(() => next())
+    .catch((err) => next(err));
+});
+
 router.use((err, req, res, next) => {
   res.status(500).json({ error: err.message });
 });
