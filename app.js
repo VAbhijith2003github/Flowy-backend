28a4a9d5fd2bda5c90d955309e98b86162efdbd6
V Abhijith <119759593+VAbhijith2003github@users.noreply.github.com>
2024-04-09 02:20:11 +0530

    vercel db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
+require('dotenv').config();
+const port =3001;
 const cors = require("cors");
 const client = require("./databasepg");
 const taskRouter = require("./routes/task");
